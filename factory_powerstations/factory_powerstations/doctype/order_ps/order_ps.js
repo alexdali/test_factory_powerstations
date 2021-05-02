8d7c541d0c3308c4c102784f5ea61e0e730e4d96
alexdali <alexdali@gmail.com>
2021-05-02 20:31:59 +0400

    Order_PS: add otification by email, add Server script, Client script

--- factory_powerstations/factory_powerstations/doctype/order_ps/order_ps.js
+++ factory_powerstations/factory_powerstations/doctype/order_ps/order_ps.js
@@ -1,21 +1,22 @@
 // Copyright (c) 2021, Alex Tas and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Order_PS', {
+// frappe.ui.form.on('Order_PS', {
 
-	on_submit(frm) {
+// 	on_submit(frm) {
 
-        // set a field as unhidden
-		//frm.set_df_property('product', 'hidden', 0);
-		// set a field as read only
-		frm.set_df_property('product', 'read_only', !frm.is_new());
+//         // set a field as unhidden
+// 		//frm.set_df_property('product', 'hidden', 0);
+// 		// set a field as read only
+// 		frm.set_df_property('product', 'read_only', !frm.is_new());
 
 
-		frappe.msgprint({
-		    title: __('Notification'),
-		    indicator: 'green',
-		    message: __(`Создан документ Станция:  ${frm.doc.product}`)
-		});
+// 		frappe.msgprint({
+// 		    title: __('Notification'),
+// 		    indicator: 'green',
+// 		    message: __(`Создан документ Станция:  ${frm.doc.product}
+// 		    	_Создан документ Станция:  `)
+// 		});
 
-    }
-});
+//     }
+// });
--- factory_powerstations/factory_powerstations/doctype/order_ps/order_ps.py
+++ factory_powerstations/factory_powerstations/doctype/order_ps/order_ps.py
@@ -3,25 +3,24 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 class Order_PS(Document):
-	# 
-	def on_submit(self):
-		### create doc power_station on submit
-		doc_power_station = frappe.get_doc({
-		    'doctype': 'Power station',
-		    'model': 'M1',
-		    'order': self.name_order,
-		    'planned_ready_date': self.date_ready
-		})
-		doc_power_station.insert()
+	pass
+	#####
+# 	def on_submit(self):
+# 		### create doc power_station on submit
+# 		doc_power_station = frappe.get_doc({
+# 		    'doctype': 'Power station',
+# 		    'model': 'M1',
+# 		    'order': self.name_order,
+# 		    'planned_ready_date': self.date_ready
+# 		})
+# 		doc_power_station.insert()
 
-		# add comment
-		self.add_comment('Comment', 'Создан документ Станция: {0}'.format(doc_power_station.name))
+# 		# add comment
+# 		self.add_comment('Comment', 'Создан документ Станция: {0}'.format(doc_power_station.name))
 
-		# link doc to field
-		self.product = doc_power_station.name
-
-
+# 		# link doc to field
+# 		self.product = doc_power_station.name
